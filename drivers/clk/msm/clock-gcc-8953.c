5d665783c9c24f76abb8b273a4549764e1bf9b6e
Cheewaca <cheewaca@gmail.com>
2018-11-13 09:50:47 +0100

    Optimization test

--- drivers/clk/msm/clock-gcc-8953.c
+++ drivers/clk/msm/clock-gcc-8953.c
@@ -385,9 +385,9 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 	F_MM( 320000000, FIXED_CLK_SRC,               gpll0,  2.5,    0,     0),
 	F_MM( 400000000, FIXED_CLK_SRC,               gpll0,    2,    0,     0),
 	F_MM( 460800000, FIXED_CLK_SRC,       gpll4_out_aux,  2.5,    0,     0),
-	F_MM( 510000000,    1020000000,               gpll3,    1,    0,     0),
-	F_MM( 650000000,    1320000000,               gpll3,    1,    0,     0),
-	F_MM( 815000000,    1500000000,               gpll3,    1,    0,     0),
+	F_MM( 560000000,    1160000000,               gpll3,    1,    0,     0),
+	F_MM( 650000000,    1340000000,               gpll3,    1,    0,     0),
+	F_MM( 815000000,    1490000000,               gpll3,    1,    0,     0),
 
 	F_END
 };
@@ -405,8 +405,8 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src_sdm450[] = {
 	F_MM( 320000000, FIXED_CLK_SRC,               gpll0,  2.5,    0,     0),
 	F_MM( 400000000, FIXED_CLK_SRC,               gpll0,    2,    0,     0),
 	F_MM( 460800000, FIXED_CLK_SRC,       gpll4_out_aux,  2.5,    0,     0),
-	F_MM( 510000000,    1020000000,               gpll3,    1,    0,     0),
-	F_MM( 560000000,    1120000000,               gpll3,    1,    0,     0),
+	F_MM( 510000000,    1060000000,               gpll3,    1,    0,     0),
+	F_MM( 560000000,    1160000000,               gpll3,    1,    0,     0),
 	F_MM( 600000000,    1200000000,               gpll3,    1,    0,     0),
 	F_END
 };
@@ -424,9 +424,9 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src_sdm632[] = {
 	F_MM( 320000000, FIXED_CLK_SRC,               gpll0,  2.5,    0,     0),
 	F_MM( 400000000, FIXED_CLK_SRC,               gpll0,    2,    0,     0),
 	F_MM( 460800000, FIXED_CLK_SRC,       gpll4_out_aux,  2.5,    0,     0),
-	F_MM( 510000000,    1020000000,               gpll3,    1,    0,     0),
-	F_MM( 560000000,    1120000000,               gpll3,    1,    0,     0),
-	F_MM( 650000000,    1300000000,               gpll3,    1,    0,     0),
+	F_MM( 510000000,    1060000000,               gpll3,    1,    0,     0),
+	F_MM( 560000000,    1160000000,               gpll3,    1,    0,     0),
+	F_MM( 650000000,    1340000000,               gpll3,    1,    0,     0),
 	F_MM( 700000000,    1400000000,               gpll3,    1,    0,     0),
 	F_MM( 725000000,    1450000000,               gpll3,    1,    0,     0),
 
